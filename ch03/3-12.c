5f646d8b73f876aba38858fc61ad843f6d1e4688
Wookshin <cotlsdnr@hanmail.net>
2017-07-03 20:45:47 +0900

    Add files via upload

---
+++ ch03/3-12.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void) {
+    struct stat buf;
+    char buff[BUFSIZ];
+
+    printf("1. stat : unix.txt ---\n");
+    stat("unix.txt",&buf);
+    printf("unix.txt : Link Count = %d\n", (int)buf.st_nlink);
+    printf("unix.txt : Inode = %d\n", (int)buf.st_ino);
+
+    printf("2. stat: unix.sym ---\n");
+    stat("unix.sym",&buf);
+    printf("unix.sym : Link Count = %d\n",(int)buf.st_nlink);
+    printf("unix.sym : Inode = %d\n",(int)buf.st_ino);
+
+    printf("3. lstat : unix.sym ---\n");
+    lstat("unix.sym",&buf);
+    printf("unix.sym : Link Count = %d\n",(int)buf.st_nlink);
+    printf("unix.sym : Inode = %d\n", (int)buf.st_ino);
+
+    realpath("unix.sym", buff);
+    printf("unix.sym : REALPATH = %s\n",buff);
+    
+    return 0;
+}
